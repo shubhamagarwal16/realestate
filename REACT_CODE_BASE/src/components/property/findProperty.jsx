3381f9e7d98281da291bd7215cdd05a74e2d681f
Shubham Agarwal <shubhamagarwal1694@gmail.com>
2019-07-02 00:49:37 +0530

    propertyFilter Page

--- REACT_CODE_BASE/src/components/property/findProperty.jsx
+++ REACT_CODE_BASE/src/components/property/findProperty.jsx
@@ -5,7 +5,49 @@ class FindProperty extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Test</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 filterBox">
+              <div>
+                <h4>
+                  Filters
+                  <a className="text-info">clear</a>
+                </h4>
+                <button className="btn btn-danger btn-sm">Search</button>
+                <div className="card">
+                  <h5>
+                    Property For:
+                    <i className="fa fa-plus-square-o float-right" />
+                  </h5>
+                  <hr className="hr" />
+                  <label htmlFor="propertyForBuy">
+                    <input
+                      type="checkbox"
+                      name="propertyFor"
+                      id="propertyForBuy"
+                    />
+                    Buy
+                  </label>
+                  <label htmlFor="propertyForSell">
+                    <input
+                      type="checkbox"
+                      name="propertyFor"
+                      id="propertyForSell"
+                    />
+                    Sell
+                  </label>
+                  <h5>
+                    Property Type:
+                    <i className="fa fa-plus-square-o float-right" />
+                  </h5>
+                  <hr className="hr" />
+                  <div className="optionsBlock" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 filteredPropertyBox" />
+          </div>
+        </div>
       </React.Fragment>
     );
   }
