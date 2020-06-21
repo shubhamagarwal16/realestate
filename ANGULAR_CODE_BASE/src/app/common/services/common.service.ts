2397ec0c62aa7cce314989e3aad9d80db8343649
Shubham Agarwal <shubham.agarwal@puresoftware.com>
2020-06-21 23:54:47 +0530

    added environment variables

--- ANGULAR_CODE_BASE/src/app/common/services/common.service.ts
+++ ANGULAR_CODE_BASE/src/app/common/services/common.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Title } from '@angular/platform-browser';
 import { Subject } from 'rxjs/Subject';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class CommonService {
@@ -27,42 +28,42 @@ export class CommonService {
 
   togglePageLoader = new Subject<boolean>();
   togglePageLoader$ = this.togglePageLoader.asObservable();
-  togglePageLoaderFn(data: boolean = false){
+  togglePageLoaderFn(data: boolean = false) {
     this.togglePageLoader.next(data);
   }
 
   // showLoader() { this.togglePageLoader.next(true); }
   // hideLoader() { this.togglePageLoader.next(false); }
 
-  main_url = 'http://localhost:8080';
-  base_url = 'http://localhost:8080/api';
+  main_url = environment.MAIN_URL;
+  base_url = environment.BASE_URL;
 
-  getStatelist(): Observable<any>{
-    return this.http.get(this.base_url+ '/common/state');
+  getStatelist(): Observable<any> {
+    return this.http.get(this.base_url + '/common/state');
   }
 
-  getCitylist(): Observable<any>{
-    return this.http.get(this.base_url+ '/common/cities');
+  getCitylist(): Observable<any> {
+    return this.http.get(this.base_url + '/common/cities');
   }
 
-  getCitylistByState(stateId): Observable<any>{
-    return this.http.get(this.base_url+ '/common/cities/'+stateId );
+  getCitylistByState(stateId): Observable<any> {
+    return this.http.get(this.base_url + '/common/cities/' + stateId);
   }
 
-  getPropertyTypeList(): Observable<any>{
+  getPropertyTypeList(): Observable<any> {
     return this.http.get(this.base_url + '/property/type');
   }
 
-  propertyList(param = ''){
+  propertyList(param = '') {
     return this.http.get(this.base_url + '/property/list/' + param);
   }
 
-  getSingleProperty(propertySlug){
+  getSingleProperty(propertySlug) {
     return this.http.get(this.base_url + '/property/single/' + propertySlug);
   }
 
-  filterProperties(param = ''){
-    return this.http.get(this.base_url + '/property/filter' + param );
+  filterProperties(param = '') {
+    return this.http.get(this.base_url + '/property/filter' + param);
   }
 
 }
