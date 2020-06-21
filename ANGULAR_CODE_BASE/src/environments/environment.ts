a5a097b40c4fc6705886f3b334c6b2f5f34e534c
Shubham Agarwal <shubhamagarwal1694@gmail.com>
2020-06-22 00:08:05 +0530

    Release nodejs (#10)
    * added angular config | other requirements
    
    * angular config
    
    * removed 'working directory'
    
    * sudo command
    
    * @angular/cli
    
    * added angualr/cli -run
    
    * chown & N command
    
    * bugfix
    
    * added NG_CLI_ANALYTICS
    
    * removed chown | fixed cli version
    
    * rebuild node-sass
    
    * sudo rebuild
    
    * reinstall node sass
    
    * added chown command
    
    * chown realestate
    
    * added ls
    
    * chown ANGULAR_CODE_BASE
    
    * install node-sass using yarn
    
    * added node-version: latest
    
    * changed docker image | removed node-sass
    
    * FInal code test 1
    
    * config nodejs
    
    * removed $CIRCLE_BRANCH
    
    * added environment variables
    
    * N- removed npm CI & console DB_URL
    
    Co-authored-by: Shubham Agarwal <shubham.agarwal@puresoftware.com>

--- ANGULAR_CODE_BASE/src/environments/environment.ts
+++ ANGULAR_CODE_BASE/src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MAIN_URL: 'http://localhost:8080',
+  BASE_URL: 'http://localhost:8080/api'
 };
