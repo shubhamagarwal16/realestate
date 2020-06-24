aebf903eee881d9dd8779e090cdd44fa7af21de4
Shubham Agarwal <shubham.agarwal@puresoftware.com>
2020-06-25 01:05:05 +0530

    A - added "server down" popup | minor fixes

--- ANGULAR_CODE_BASE/src/app/app.component.ts
+++ ANGULAR_CODE_BASE/src/app/app.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@sa-environments/environment';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'server-error-modal',
+  template: `
+    <div>
+      <div class="modal-header justify-content-center">
+        <h4 class="modal-title" id="modal-basic-title">Alert</h4>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-warning" role="alert">
+          Their seems to be an issue with the server, try <a href="/">reloading</a>.
+        </div>
+      </div>
+    </div>
+  `
+})
+export class ServerDownModal {
+  constructor(public activeModal: NgbActiveModal) { }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +28,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    private _http: HttpClient,
+    private modalService: NgbModal
+  ) {
+    const url = environment.BASE_URL.split('/api')[0];
+    this._http.get(url)
+      .subscribe(response => {
+      }, (error) => {
+        console.log(error)
+        if (error.status !== 200) {
+          alert('not working')
+        }
+        this.openErrorModal()
+      })
+  }
+
+  openErrorModal() {
+    this.modalService.open(ServerDownModal)
+  }
 }
